00b1abbdefe8d2672de88fb4a05611e4157ebce9
jvillacanass <jvillacanass@gmail.com>
2017-12-04 14:04:43 +0100

    primera subida

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express'),
+    fs = require('fs'),
+    app = express();
+
+var app = express();
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', function(req, res) {
+    res.send('Hello from Ingest-bikes  at '+ new Date());
+});
+
+app.listen(8080, ip);
+
+module.exports = app;
+
+var Client = require('node-rest-client').Client;
+var client = new Client();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://jvillacanass:Or3VR3XdxTjEYQYw@cluster0-shard-00-00-4fuwm.mongodb.net:27017,cluster0-shard-00-01-4fuwm.mongodb.net:27017,cluster0-shard-00-02-4fuwm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+
+
+
+var cron = require('node-cron');
+
+cron.schedule('*/5 * * * *', function(){
+var estaciones;
+MongoClient.connect(url, function(err, db) {
+	
+  if (err) throw err;	
+	client.get("http://api.citybik.es/v2/networks/bicing?fields=stations", function (data, response) {		
+		estaciones = null;
+		for (var id in data) {				
+			estaciones = JSON.stringify(data[id]["stations"]);					
+		}		
+		estaciones = JSON.parse(estaciones);
+		db.collection("stations").insertMany(estaciones, function(err, res) {
+			if (err) throw err;
+			require('console-stamp')(console, '[ddd mmm dd yyyy HH:MM:ss]');
+			console.log("Filas insertadas: " + res.insertedCount);			
+			db.close();
+		});			
+	});
+});
+});
