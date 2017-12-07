af72667c79226ec2fd27cce638a16cb64cae2f57
jvillacanass <jvillacanass@gmail.com>
2017-12-07 08:20:04 +0100

    arreglo

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ var client = new Client();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://jvillacanass:Or3VR3XdxTjEYQYw@cluster0-shard-00-00-4fuwm.mongodb.net:27017,cluster0-shard-00-01-4fuwm.mongodb.net:27017,cluster0-shard-00-02-4fuwm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 
-
-
 var cron = require('node-cron');
 
 cron.schedule('*/5 * * * *', function(){
