d07fd8238b27a63fe15cb88b8ae7166380d052b1
jvillacanass <jvillacanass@gmail.com>
2017-12-18 18:39:44 +0100

    cambio para parar la ingesta

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ module.exports = app;
 var Client = require('node-rest-client').Client;
 var client = new Client();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://jvillacanass:Or3VR3XdxTjEYQYw@cluster0-shard-00-00-4fuwm.mongodb.net:27017,cluster0-shard-00-01-4fuwm.mongodb.net:27017,cluster0-shard-00-02-4fuwm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+var url = "mongodb://jvillacanass:XXXOr3VR3XdxTjEYQYw@cluster0-shard-00-00-4fuwm.mongodb.net:27017,cluster0-shard-00-01-4fuwm.mongodb.net:27017,cluster0-shard-00-02-4fuwm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 
 var cron = require('node-cron');
 
